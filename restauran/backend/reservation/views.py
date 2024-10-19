a22fda11196dcc3fb3f4564ce9126b2e4e08dd23
AxmedovAziz <aziz9259657@gmail.com>
2024-10-19 17:57:28 -0400

    firstper

--- restauran/backend/reservation/views.py
+++ restauran/backend/reservation/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ReservationForm, UpdateReservation
 from .models import Reservation
-from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.decorators import permission_required, login_required
 
 
 def reservation_view(request):
@@ -38,10 +38,19 @@ def update_reservation_view(request, pk: int):
 
 
 # @permission_required("reservation.delete_mymodel")
+@login_required
 def delete_reservation_view(request, pk: int):
+    # Get the reservation object
     reservation = get_object_or_404(Reservation, id=pk)
+
     if request.method == "POST":
-        if reservation.name == request.user.username:
+        # Check if the user is either the owner of the reservation or has the delete permission
+        if reservation.name == request.user.username or request.user.has_perm(
+            "reservation.delete_reservation"
+        ):
+            # Delete the reservation if allowed
             reservation.delete()
             return redirect("blog")
+
+    # Render the delete confirmation page
     return render(request, "confirm_delete.html", {"reservation": reservation})
